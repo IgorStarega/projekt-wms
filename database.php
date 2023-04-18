49707fce4a06af23157b973c03642b107f923543
IgorStarega <staregaigor@gmail.com>
2023-04-18 18:51:36 +0200

    start

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+include_once('config.php');
+
+function getConnection() {
+    global $dbHost, $dbName, $dbUser, $dbPassword;
+    $connection = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+    if($connection->connect_errno) {
+        $connection->close();
+        die('Database connection problem');
+    }
+    return $connection;
+}
